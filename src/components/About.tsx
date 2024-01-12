164b3cfee0776ecc392791c4a02bfb5150337ccc
Vo Van Mai <mai.vv@neo-lab.vn>
2024-01-12 08:35:45 +0700

    edit menu

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { Button, Drawer } from 'antd';
+import { FC, useState } from 'react';
+import { Button, Drawer, Menu } from 'antd';
+import {AppstoreOutlined} from "@ant-design/icons";
 
-const About: React.FC = () => {
+const About: FC = () => {
   const [open, setOpen] = useState(false);
 
   const showDrawer = () => {
@@ -17,10 +18,40 @@ const About: React.FC = () => {
       <Button type="primary" onClick={showDrawer}>
         Open
       </Button>
-      <Drawer title="Basic Drawer" placement="left" onClose={onClose} open={open}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+      <Drawer
+        title="Basic Drawer"
+        width={300}
+        styles={{
+          body: { padding: 0}
+        }}
+        placement="left"
+        onClose={onClose}
+        open={open}
+      >
+        <Menu
+          style={{flex: 1, overflowY: "auto"}}
+          defaultOpenKeys={['1']}
+          defaultSelectedKeys={['1']}
+          mode="inline"
+        >
+          <Menu.SubMenu key="1" title="Settings" icon={<AppstoreOutlined/>}>
+            <Menu.Item key="2">Option 1</Menu.Item>
+            <Menu.Item key="3">Option 2</Menu.Item>
+            <Menu.SubMenu key="4" title="Sub-Menu">
+              <Menu.Item key="5">Option 3</Menu.Item>
+              <Menu.Item key="6">Option 4</Menu.Item>
+            </Menu.SubMenu>
+          </Menu.SubMenu>
+          <Menu.SubMenu key="7" title="Profile">
+            <Menu.Item key="8">Option 5</Menu.Item>
+            <Menu.Item key="9">Option 6</Menu.Item>
+            <Menu.Item key="10">Option 7</Menu.Item>
+            <Menu.Item key="11">Option 8</Menu.Item>
+          </Menu.SubMenu>
+          {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21].map((number) =>
+            <Menu.Item key={number}>Option {number}</Menu.Item>
+          )}
+        </Menu>
       </Drawer>
     </>
   );
