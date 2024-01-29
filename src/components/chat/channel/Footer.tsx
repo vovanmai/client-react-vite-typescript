867b511fe66c719773c20921fad9016aa50c4ffd
Vo Van Mai <mai.vv@neo-lab.vn>
2024-01-29 10:38:15 +0700

    chat

--- src/components/chat/channel/Footer.tsx
+++ src/components/chat/channel/Footer.tsx
@@ -1,26 +1,32 @@
-import {Button, Form, Input} from "antd"
+import {Button, Form, Input, message} from "antd"
 import {SendOutlined} from "@ant-design/icons"
 import { useRef } from "react";
 import type { InputRef } from 'antd';
 
 const Footer = () => {
   const messageRef = useRef<InputRef>(null)
+  const [messageApi, contextHolder] = message.useMessage();
   const [form] = Form.useForm();
-  const onFinish = () => {
-    messageRef.current?.focus()
-    form.setFieldValue('message', '');
-  };
-
-  const onFinishFailed = () => {
+  const onFinish = (value: any) => {
+    const message = value.message.trim()
+    if (message !== '') {
+      messageRef.current?.focus()
+      form.setFieldValue('message', '');
+    } else {
+      messageApi.open({
+        type: 'error',
+        content: 'Vui lòng nhập trước khi gửi!',
+      });
+    }
   };
   return (
       <div style={{borderTop: "1px solid #e1dbdb", height: 60, padding: "0 5px" }} className="d-flex align-item-center">
+        {contextHolder}
         <Form
           form={form}
           className="w-100"
           name="basic"
           onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
           autoComplete="off"
           layout="inline"
         >
--- src/views/chat/Channel.tsx
+++ src/views/chat/Channel.tsx
@@ -1,14 +1,28 @@
 import Header from "@/components/chat/channel/Header";
 import Footer from "@/components/chat/channel/Footer";
 import dataChannels from "@/configs/channels";
-import {useParams} from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { keyBy } from 'lodash'
-
+import {useEffect, useState} from "react";
 
 const Channel = () => {
   const params = useParams()
-  const channels = keyBy(dataChannels, 'id')
-  const channel = channels[params.id ?? '']
+  const navigate = useNavigate()
+  const [channel, setChannel] = useState({
+    id: 0,
+    name: '',
+  })
+
+  useEffect(() => {
+    const channels = keyBy(dataChannels, 'id')
+    const channelItem = channels[params.id ?? '']
+    if (channelItem) {
+      setChannel(channelItem)
+    } else {
+      navigate('/chat')
+    }
+  })
+
   return (
     <div className="w-100 h-100 d-flex" style={{ flexDirection: "column"}}>
       <Header channel={channel}/>
