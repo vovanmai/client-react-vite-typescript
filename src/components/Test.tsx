c13353271e2e5a873a426b88faaac1d4a2a4fb0f
Vo Van Mai <mai.vv@neo-lab.vn>
2024-02-19 11:31:17 +0700

    test

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,28 +1,84 @@
-import { useState } from "react";
-const fakeApi = () => console.log('Api is called')
-
+import {useEffect, useState} from "react";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Message from "@/request/Message";
 const Test = () => {
-  const [inputValue, setInputValue] = useState('')
-  const [timer, setTimer] = useState<any>(null)
+  const [messages, setMessages] = useState<any>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [minId, setMinId] = useState<any>(null)
+  const [hasMoreData, setHasMoreData] = useState<any>(true)
 
-  const inputChanged = (e: any) => {
-    setInputValue(e.target.value)
+  const direction:string = 'top'
+  // const direction:string = 'bottom'
 
-    clearTimeout(timer)
+  useEffect(() => {
+    setIsLoading(true)
+    Message.list(1).then((response: any) => {
+      let apiMessages = response.data
+      if (direction === 'top') {
+        apiMessages = apiMessages.reverse()
+        setMessages([...apiMessages, ...messages])
+        setMinId(apiMessages.length > 0 ? apiMessages[0].id : null)
+        const element:any = document.getElementById('test')
+        element.scrollTo(0, 500)
+      } else {
+        setMessages([...messages, ...apiMessages])
+        setMinId(apiMessages.length > 0 ? apiMessages[apiMessages.length - 1].id : null)
+        const element:any = document.getElementById('test')
+        element.scrollTo(0, 1)
+      }
+      setHasMoreData(apiMessages.length === 20)
+      setIsLoading(false)
+    })
+  }, [])
 
-    const newTimer = setTimeout(() => {
-      fakeApi()
-    }, 500)
+  const handleScroll = (e: any) => {
+    if (isLoading) return
 
-    setTimer(newTimer)
-  }
+    const el = e.target;
+    let check: boolean
 
-  return (
+    if (direction === 'top') {
+      check = el.scrollTop === 0 && hasMoreData
+    } else {
+      check = el.scrollTop + el.clientHeight >= el.scrollHeight && hasMoreData
+    }
+
+    if (check) {
+      setIsLoading(true)
+      Message.list(1, {min_id: minId}).then((response: any) => {
+        let apiMessages = response.data
+        if (direction === 'top') {
+          apiMessages = apiMessages.reverse()
+          setMessages((oldArray: any) => oldArray.unshift(...apiMessages));
+          setMinId(apiMessages.length > 0 ? apiMessages[0].id : null)
+        } else {
+          setMessages([...messages, ...apiMessages])
+          setMinId(apiMessages.length > 0 ? apiMessages[apiMessages.length - 1].id : null)
+        }
+        setIsLoading(false)
+        setHasMoreData(apiMessages.length === 20)
+      })
+    }
+  };
+
+  useEffect(() => {
+    const element:any = document.getElementById('test')
+    element.addEventListener('scroll', handleScroll);
+    return () => element.removeEventListener('scroll', handleScroll);
+  }, [isLoading]);
+
+  return(
     <div>
-      <label>Input: </label>
-      <input value={inputValue} type="text" onChange={inputChanged} />
+      { direction === 'top' && isLoading && <div>Loading...</div> }
+      { direction === 'top' && !hasMoreData && <div>No data...</div> }
+      <div id="test" style={{height: 500, overflow: "auto"}}>
+        {messages.map((item:any) => {
+          return <div style={{border: "1px solid gray", padding: "10px 0px"}} key={item.id}>{item.id}</div>
+        })}
+      </div>
+      { direction === 'bottom' && isLoading && <div>Loading...</div> }
+      { direction === 'bottom' && !hasMoreData && <div>No data...</div> }
     </div>
   )
 }
-
 export default Test
